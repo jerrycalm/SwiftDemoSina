0b3b1650135575b14f0002bba5759a7aa0c3f3e7
Dylan <qingpeng.qp.wang@gtomato.com>
2019-06-03 14:21:58 +0800

    setup pj

---
+++ SinaDemo/SinaDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SinaDemo
+//
+//  Created by Dylan on 2019/6/3.
+//  Copyright © 2019 GT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
